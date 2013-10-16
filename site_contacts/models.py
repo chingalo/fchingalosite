2ae616de366dce6223570e1038acfb96043112d8
chingalo <profschingalo@gmail.com>
2013-10-16 11:16:08 +0300

    add site contacts app

---
+++ site_contacts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from cms.models.pluginmodel import CMSPlugin
+ 
+class Contacts(models.Model):
+	first_name = models.CharField(max_length=200, blank=True)
+	middle_name = models.CharField(max_length=200, blank=True)
+	surname = models.CharField(max_length=200, blank=True)
+	e-mail = models.EmailField(max_length=200,  blank=True)
+	mobile_phone = models.CharField(max_length=20, blank=True, default='+255')
+	location = models.CharField(max_length=200, blank=True)
+	duty = models.TextField(max_length=2000, blank=True)
+	message = models.TextField(max_length=20000000, blank=True)
+	
+	def __unicode__(self):   # Python 3: def __str__(self):
+		return self.first_name
+
+#class ContactsPlugin(CMSPlugin):
+	#title=models.CharField(max_length=200, default='contacts ')
---
+++ site_contacts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+#import apps views
+urlpatterns = patterns('',
+#example:  url(r'^$', views.index, name='view_all_polls'),
+
+
+)
