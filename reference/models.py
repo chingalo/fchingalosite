93486ba341e84042991271833d9e16504b31936e
chingalo <profschingalo@gmail.com>
2014-05-19 04:38:04 +0300

    add refrence category for easy organization of refrences

--- reference/models.py
+++ reference/models.py
@@ -4,6 +4,15 @@ from cms.models.pluginmodel import CMSPlugin
 class Reference(models.Model):
 	reference_title = models.CharField(max_length=200)
 	reference_title_discription = models.TextField(max_length=2000, blank=True)
+	
+	#refernces categories
+	category_choice = (
+	('Finance','Finance'),
+	('Institution','Institution'),
+	('Social','Social'),
+	('Others','Others'),	
+	)
+	reference_category = models.CharField(max_length=200, choices = category_choice , default = 'Finance')
 		
 	def __unicode__(self):  # Python 3: def __str__(self):
 		return self.reference_title
