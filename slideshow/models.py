902d4e9973afc09214275b5d2567fd0e4b597063
chingalo <profschingalo@gmail.com>
2014-05-19 06:57:53 +0300

    create model for slide shows

--- slideshow/models.py
+++ slideshow/models.py
@@ -2,7 +2,7 @@ from cms.models.pluginmodel import CMSPlugin
 from django.db import models
 
 # Create your models here.
-class Slideshow(models.Model):
+class SlideshowHome(models.Model):
     image = models.ImageField(upload_to='carousel')
     desc_title = models.CharField(max_length=200,default='')
     desc_text = models.CharField(max_length=500,default='')
@@ -10,6 +10,38 @@ class Slideshow(models.Model):
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.desc_title
 
-class SlideshowPlugin(CMSPlugin):
-    title = models.CharField(max_length=200,default='my slide show')
+class SlideshowReference(models.Model):
+    image = models.ImageField(upload_to='carousel')
+    desc_title = models.CharField(max_length=200,default='')
+    desc_text = models.CharField(max_length=500,default='')
     
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.desc_title        
+
+class SlideshowReport(models.Model):
+    image = models.ImageField(upload_to='carousel')
+    desc_title = models.CharField(max_length=200,default='')
+    desc_text = models.CharField(max_length=500,default='')
+    
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.desc_title
+        
+class SlideshowSearch(models.Model):
+    image = models.ImageField(upload_to='carousel')
+    desc_title = models.CharField(max_length=200,default='')
+    desc_text = models.CharField(max_length=500,default='')
+    
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.desc_title 
+               
+class SlideshowHomePlugin(CMSPlugin):
+    title = models.CharField(max_length=200,default='home page slide show')
+    
+class SlideshowReferencePlugin(CMSPlugin):
+    title = models.CharField(max_length=200,default='reference page slide show')
+    
+class SlideshowReportPlugin(CMSPlugin):
+    title = models.CharField(max_length=200,default='report page slide show')    
+
+class SlideshowSearchPlugin(CMSPlugin):
+    title = models.CharField(max_length=200,default='search page slide show')
