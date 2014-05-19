c57c42bc818f739db6643069167050021e1df116
chingalo <profschingalo@gmail.com>
2014-05-19 06:58:52 +0300

    create plugins for slideshowas

--- slideshow/cms_plugins.py
+++ slideshow/cms_plugins.py
@@ -4,18 +4,63 @@ from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 from slideshow.models import  *
 
-class SlidePlugin(CMSPluginBase):
-    model = SlideshowPlugin
-    name = "Slideshow Plugin"
+class SlideshowHome(CMSPluginBase):
+    model = SlideshowHomePlugin
+    name = "Home slideshow Plugin"
     render_template = "plugins/slideshow_plugin.html"
     
     def render(self, context, instance, placeholder):
-        latest = Slideshow.objects.all()
+        images = SlideshowHome.objects.all()
         context.update({
             'instance': instance,
-            'latest': latest,
+            'images': images,
             'placeholder': placeholder,
         })
         return context
 
-plugin_pool.register_plugin(SlidePlugin)
+class SlideshowReference(CMSPluginBase):
+    model = SlideshowReferencePlugin
+    name = "Reference slideshow Plugin"
+    render_template = "plugins/slideshow_plugin.html"
+    
+    def render(self, context, instance, placeholder):
+        images = SlideshowReference.objects.all()
+        context.update({
+            'instance': instance,
+            'images': images,
+            'placeholder': placeholder,
+        })
+        return context
+        
+class SlideshowReport(CMSPluginBase):
+    model = SlideshowReportPlugin
+    name = "Report slidesho Plugin"
+    render_template = "plugins/slideshow_plugin.html"
+    
+    def render(self, context, instance, placeholder):
+        images = SlideshowReport.objects.all()
+        context.update({
+            'instance': instance,
+            'images': images,
+            'placeholder': placeholder,
+        })
+        return context
+        
+class SlideshowSearch(CMSPluginBase):
+    model = SlideshowSearchPlugin
+    name = "Search slideshow Plugin"
+    render_template = "plugins/slideshow_plugin.html"
+    
+    def render(self, context, instance, placeholder):
+        images = SlideshowSearch.objects.all()
+        context.update({
+            'instance': instance,
+            'images': images,
+            'placeholder': placeholder,
+        })
+        return context
+                        
+plugin_pool.register_plugin(SlideshowHome)
+plugin_pool.register_plugin(SlideshowReference)
+plugin_pool.register_plugin(SlideshowReport)
+plugin_pool.register_plugin(SlideshowSearch)
