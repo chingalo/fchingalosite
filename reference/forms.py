42c644d4908a49a71a026215bb2e71e041dd2eb7
chingalo <profschingalo@gmail.com>
2014-05-20 05:12:33 +0300

    add forms for upload purpose

--- reference/forms.py
+++ reference/forms.py
@@ -1,10 +1,14 @@
 from django import forms
-from cms.models.pluginmodel import CMSPlugin
 from reference.models import *
 
-class referenceForm(forms.ModelForm):
+class referenceStep1(forms.ModelForm):
+	class Meta:
+		model = Reference
+
+class referenceStep2(forms.ModelForm):
 	class Meta:
 		model = Reference_file
 
-class ReferenceFormPlugin(CMSPlugin):
-	title=models.CharField(max_length=200, default='test fro form')
+
+
+
--- report/forms.py
+++ report/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+from report.models import *
+
+class reportStep1(forms.ModelForm):
+	class Meta:
+		model = Receiver_of_report
+
+class reportStep2(forms.ModelForm):
+	class Meta:
+		model = Details_of_Report
+		
+class reportStep3(forms.ModelForm):
+	class Meta:
+		model = Report_files		
