9a15b7c1793ee3e94ff8a98d0c149bb6c1576e9c
chingalo <profschingalo@gmail.com>
2014-05-21 01:10:31 +0300

    add urls for other steps during uploading references and repost

--- report/urls.py
+++ report/urls.py
@@ -4,8 +4,8 @@ from report import views
 
 urlpatterns = patterns('',
     url(r'^$', views.reportUploadStep1, name='reportStep1'),
-    #url(r'step1^$', views.reportUploadStep2, name='reportStep2'),
-    #url(r'step1^$', views.reportUploadStep3, name='reportStep3'),
+    url(r'^step2$', views.reportUploadStep2, name='reportStep2'),
+    url(r'^step3$', views.reportUploadStep3, name='reportStep3'),
 
 #url(r'step1^$', views.authorize, name='authorize'),
 )
