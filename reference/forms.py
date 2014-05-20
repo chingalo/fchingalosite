7bf2a0f8bcc6b1699f2a69a1380667980f0beab9
chingalo <profschingalo@gmail.com>
2014-05-21 01:09:32 +0300

    update forms for reference uplaod

--- reference/forms.py
+++ reference/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from reference.models import *
 
-class referenceStep1(forms.ModelForm):
+class referenceStep1Form(forms.ModelForm):
 	class Meta:
 		model = Reference
 
-class referenceStep2(forms.ModelForm):
+class referenceStep2Form(forms.ModelForm):
 	class Meta:
 		model = Reference_file
 
