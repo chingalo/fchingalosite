aeb5490fb013b68873170832c487077b8faaa4a3
chingalo <profschingalo@gmail.com>
2014-05-22 02:15:44 +0300

    add urls for all functionalities

--- reference/urls.py
+++ reference/urls.py
@@ -4,7 +4,7 @@ from reference import views
 urlpatterns = patterns('',
     url(r'^referenceupload/$', views.referenceUploadStep1, name='referenceStep1'),
     url(r'^referenceupload/step2$', views.referenceUploadStep2, name='referenceStep2'),
-    
-    
+    url(r'^download/$', views.referenceDownload, name='referenceDownload'),
+    url(r'^search/$', views.referenceSearch, name='referenceSearch'),    
     
     )
--- report/urls.py
+++ report/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     url(r'^reportupload/$', views.reportUploadStep1, name='reportStep1'),
     url(r'^reportupload/step2$', views.reportUploadStep2, name='reportStep2'),
     url(r'^reportupload/step3$', views.reportUploadStep3, name='reportStep3'),
+    url(r'^download/$', views.reportDownload, name='reportDownload'),
+    url(r'^search/$', views.reportSearch, name='reportSearch'),
 
 #url(r'step1^$', views.authorize, name='authorize'),
 )
